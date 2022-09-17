840be89d9614965ba410bb249597bca55122cf6f
Tuna <tunoz.23@yahoo.com>
2022-09-17 09:51:33 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 // Created by TUN on 13.09.2022.
 //
 #include <iostream>
+#include <string>
+
+class Player{
+private:
+    std::string m_Name;
+public:
+    Player(const std::string& name)
+        : m_Name(name) {}
+
+};
 
 int main()
 {
