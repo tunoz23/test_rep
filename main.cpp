c1e54bd815c3fc9070720c197bd84f4b161d725b
Tuna <tunoz.23@yahoo.com>
2022-09-22 00:31:24 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 // Created by TUN on 13.09.2022.
 //
 #include <iostream>
+#include <vector>
 #define LOG(a) cout << a << endl
 using namespace std;
 
-void math(const int* a){
-  LOG(a);
+void math(int arr_1){
+
+    LOG(arr_1);
 }
 
 int main()
 {
-
+    int int_array[] = {10, 20, 30 ,40};
+    int a = 10;
+    math(a);
     return 0;
 }
