40fffbfd43c5b231cb43f37e3eea2528ed7c0c74
Tuna <tunoz.23@yahoo.com>
2022-09-22 10:43:46 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <vector>
+#include <cmath>
 #define LOG(a) cout << a << endl
 using namespace std;
 
@@ -21,11 +22,53 @@ int getAbsSum(const vector<int>& vect){
 
     return abs_sum;
 }
+int triaunglar(int dot){
+    int total = 0;
+    for (int i = dot; i>0; i--, dot--){
+        total += dot;
+    }
+    return total;
+}
+bool lastDig(int a, int b, int c) {
+    int ld_a = a % 10;
+    int ld_b = b % 10;
+    int ld_c = c % 10;
+    int ld_ab = (ld_a * ld_b) % 10;
+    if (ld_ab == ld_c){
 
+     return true;
+    }
+    else{
+        return false;
+    }
+}
+double quadraticEquation(int a, int b, int c) {
+    int discr = b*b - 4 * a *c;
+    double root =  (-b + sqrt(discr)) / (2 * a);
+    return root;
+}
+//It(test10){Assert::That(cars(97, 6, 10), Equals(5));} = 6
+int cars(int wheels, int bodies, int figures) {
+    //4 wheels, 1 car body, and 2 figures
+    int wh = wheels / 4 - ((wheels % 4)/ 4);
+    //int bd = bodies / 1 - ((bodies % 1)/ 1);
+    int fig = figures / 2 - ((figures % 2)/ 2);
+    if (wh < bodies and wh < fig){
+        return wh;
+
+    }
+    else if (bodies < wh and bodies < fig){
+        return bodies;
+
+    }
+    else{
+        return fig;
+    }
+}
 int main()
 {
-    vector<int> v1 = {-10, 20, 30 ,40};
-    int a = 10;
-    LOG(math(v1));
+
+    cars(10,20,30);
+
     return 0;
 }
